d5f46f6f5d71066ef08159bfd50b859bc0b8b6a3
Jesper Nielsen <jesper@windelborg.dk>
2016-10-28 13:07:33 +0200

    Add a small test server Adding a small server script, it can listen on a port and repeat everything it recives.

---
+++ server.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import asyncio
+
+host = '127.0.0.1'
+port = '8000'
+
+@asyncio.coroutine
+def echo_server():
+    yield from asyncio.start_server(handle_connection, host, port)
+
+@asyncio.coroutine
+def handle_connection(reader, writer):
+    while True:
+        data = yield from reader.read(8192)
+        if not data:
+            break
+        writer.write(data)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(echo_server())
+
+try:
+    loop.run_forever()
+finally:
+    loop.close()
